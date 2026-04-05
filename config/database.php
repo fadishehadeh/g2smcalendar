792227a2225b8e30c641cba144fab791d90e0af7
fadishehadeh <fshehadeh@gmail.com>
2026-04-05 11:53:13 +0300

    Add config/database.php

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'g2_social_media_calendar',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+];
