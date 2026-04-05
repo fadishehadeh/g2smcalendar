588b7c4792f40a2b9c48670ebed15ddc99bd7b72
fadishehadeh <fshehadeh@gmail.com>
2026-04-05 11:51:42 +0300

    Add app/Views/errors/404.php

---
+++ app/Views/errors/404.php
@@ -0,0 +1,6 @@
+<?php require dirname(__DIR__) . '/partials/header.php'; ?>
+<div class="panel empty-state">
+    <h3>Not Found</h3>
+    <p>The requested page does not exist.</p>
+</div>
+<?php require dirname(__DIR__) . '/partials/footer.php'; ?>
