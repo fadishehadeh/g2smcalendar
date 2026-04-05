8b9ed1246a4eda7fbe3b17de7ad821deb047c8de
fadishehadeh <fshehadeh@gmail.com>
2026-04-05 11:51:31 +0300

    Add app/Views/errors/403.php

---
+++ app/Views/errors/403.php
@@ -0,0 +1,6 @@
+<?php require dirname(__DIR__) . '/partials/header.php'; ?>
+<div class="panel empty-state">
+    <h3>Forbidden</h3>
+    <p>You do not have access to this area.</p>
+</div>
+<?php require dirname(__DIR__) . '/partials/footer.php'; ?>
